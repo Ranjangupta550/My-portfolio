1acd9ca39521c112085104c27d50e587e7e88808
RanjanGupta550 <ranjanrider550@gmail.com>
2024-01-25 17:14:08 +0530

    next parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./app.scss"
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return <div>
     <section id="Homepage">
       <Navbar/>
+      <Hero/>
     </section>
     <section>Parallax</section>
     <section>Services</section>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
     <div className="navbar">
       <Sidebar/>
       <div className="wapper">
-        <motion.span initial={{opacity:0,scale:0 }} animate={{opacity:1,scale:1 }} transition={{duration:1}}>Ranjan Gupta</motion.span>
+        <motion.span className="Name" initial={{opacity:0,scale:0 }} animate={{opacity:1,scale:1 }} transition={{duration:1}}>Ranjan Gupta</motion.span>
         <div className="social">
        
-          <a href="#"><img src="/instagram.png"alt="" /></a>
-          <a href=""><img src="/facebook.png" alt="Ranjan" /></a>
-          <a href=""><img src="/linkedin.png" alt="Ranjan" /></a>
-          <a href=""><img src="/github.png" alt="Ranjan" /></a>
-          <a href=""><img src="/twitter.png" alt="Ranjan" /></a>
+          <a href="https://www.instagram.com/dranjan2104?igsh=c2NjZmZtdG14M25s"><img src="/instagram.png"alt="instagram" /></a>
+          <a href="https://www.facebook.com/ranjan143dd?mibextid=ZbWKwL"><img src="/facebook.png" alt="facebook" /></a>
+          <a href="https://www.linkedin.com/in/ranjan-gupta-1a1b13250/"><img src="/linkedin.png" alt="linkedin" /></a>
+          <a href="https://github.com/Ranjangupta550"><img src="/github.png" alt="github" /></a>
+          <a href="https://www.linkedin.com/in/ranjan-gupta-1a1b13250?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src="/twitter.png" alt="github" /></a>
         
          
         </div>
